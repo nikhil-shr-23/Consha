d99ecf53b84181e5ef7bb6e5aa343a2b3cd350f6
Nikhil Sharma <nikhilijs@gmail.com>
2025-09-28 03:02:16 +0530

    pnpm shadcn initiated

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-4xl">Consha</h1>
+      <Button>What is this?</Button>
 
     </div>
   );
